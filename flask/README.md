97db301ec93d245916041eab35faf2c4f585ffdd
Oliver Guggenbuehl <o.guggenbuehl@gmail.com>
2015-10-19 00:00:21 +0200

    init docker fun2

--- flask/README.md
+++ flask/README.md
@@ -4,3 +4,6 @@ docker build --rm=true -t flaskapp .
 
 docker run -d -p 80:8033 flaskapp
  
+
+one liner
+appname=$(docker ps |grep -i flaskapp | awk '{ print $1}'); docker stop ${appname} ; docker rm ${appname} ; docker rmi -f flaskapp; docker build --rm=true -t flaskapp . ; docker run -d -p 8033:80 flaskapp
--- flask/app/hello.py
+++ flask/app/hello.py
@@ -1,12 +1,14 @@
 #!flask/bin/python
 from flask import render_template
 from flask import Flask
-
+import os
 app = Flask(__name__)
+
+env = os.environ
 
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    return render_template('hello.html', env=env)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
